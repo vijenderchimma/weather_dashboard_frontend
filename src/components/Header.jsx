039b2934ecb3e275fe34520366186f65531bed58
vijender chimma <vijenderchimma424@gmail.com>
2024-06-08 18:27:46 +0530

    click on nav header return to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <nav className="header-container">
-        <h1 className='website-logo'>Weather DashBoard</h1> 
+        <h1 className='website-logo'><Link to ="/" className='nav-link'>Weather DashBoard</Link></h1> 
         <ul className='link-container'>
             <li><Link to = "/" className = "nav-link">Home</Link></li>
             <li><Link to = "/map" className = "nav-link">Map</Link></li>
